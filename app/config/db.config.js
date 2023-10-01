a8f48364bd7997aac0f481aca410a4260ca50e57
Jay <koonjjkung.castgame@gmail.com>
2023-10-02 00:16:54 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports ={
+    HOST: "sql.freedb.tech",
+    USER: "freedb_jay",
+    PASSWORD: "RDx&8DqR&h53zat",
+    DB: "freedb_homework3",
+    PORT: 3306,
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+global.__basedir = __dirname;
+var corsOptions = {
+    origin: "*"
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res)=>{
+    res.json({ message: "Welcome to my Manage Account API. I'm Chayanon Rodkeaw 6430613015 :D"});
+});
+require("./app/routes/user.routes")(app);
+require("./app/routes/file.routes")(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("Server is running on port " + PORT);
+});
